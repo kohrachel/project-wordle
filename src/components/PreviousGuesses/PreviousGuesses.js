4fdad0ed077864fa00d44d0e046322e83a2d88c6
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-07-29 14:34:45 -0400

    Challenge 3: Add cell boxes for guesses, attempt 1 (messy)

--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { range } from "../../utils";
+
+const NUM_ROWS = 6;
+const NUM_COLS = 5;
 
 function PreviousGuesses({ guessHistory }) {
   return (
     <div className="guess-results">
-      {guessHistory.map(({ id, label }) => (
-        <p key={id} className="guess">
-          {label}
+      {range(0, NUM_ROWS).map((numGuess, index) => (
+        <p className="guess" key={index}>
+          {guessHistory[numGuess]
+            ? range(0, NUM_COLS).map((_, index) => (
+                <span className="cell" key={index}>
+                  {guessHistory[numGuess].label[index]}
+                </span>
+              ))
+            : range(0, NUM_COLS).map((_, index) => (
+                <span className="cell" key={index}></span>
+              ))}
         </p>
       ))}
     </div>
