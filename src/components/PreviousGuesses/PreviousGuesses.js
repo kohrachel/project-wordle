38224ad505b372664378daa8c33467a294172c77
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-07-29 15:30:55 -0400

    Refactor some more, mainly just trying to get a better understanding of how these things work. Or, I fight JS over its syntax and lose

--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -3,16 +3,21 @@ import { range } from "../../utils";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants.js";
 
 function PreviousGuesses({ guessHistory }) {
+  const GuessRow = ({ numGuess }) => {
+    return (
+      <p className="guess">
+        {range(5).map((_, cellIndex) => (
+          <span className="cell" key={cellIndex}>
+            {guessHistory[numGuess]?.[cellIndex]}
+          </span>
+        ))}{" "}
+      </p>
+    );
+  };
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((numGuess) => (
-        <p className="guess" key={numGuess}>
-          {range(5).map((_, cellIndex) => (
-            <span className="cell" key={cellIndex}>
-              {guessHistory[numGuess]?.[cellIndex]}
-            </span>
-          ))}
-        </p>
+        <GuessRow key={numGuess} numGuess={numGuess} />
       ))}
     </div>
   );
